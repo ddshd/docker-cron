0b7bd85ee12bb7a609423fa19289796a874f26f0
Dhrumil Shah <ddshd@users.noreply.github.com>
2024-05-16 00:40:23 -0400

    Output to stdout/err as it happens

--- runner.py
+++ runner.py
@@ -2,6 +2,7 @@ import datetime
 import time
 import glob
 import subprocess
+import select
 from croniter import croniter
 import os
 
@@ -9,10 +10,28 @@ def run_tasks():
     scripts = glob.glob('/scripts/*.sh')
     for script in scripts:
         print(f"[Running: {script}]")
-        result = subprocess.run([script], shell=True, text=True, capture_output=True)
-        print("Output:", result.stdout)
-        if result.stderr:
-            print("Error:", result.stderr)
+        process = subprocess.Popen([script], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+        # Create a pipe set to monitor both stdout and stderr
+        pipes = [process.stdout, process.stderr]
+        while True:
+            # Use select to wait for data to become available on any of the pipes
+            readable, _, _ = select.select(pipes, [], [])
+            for pipe in readable:
+                # Read a line from the pipe
+                line = pipe.readline()
+
+                # If the pipe has been closed, remove it from the list
+                if line == '':
+                    pipes.remove(pipe)
+                else:
+                    # Print the line as it comes
+                    print(line, end='')
+            # Break the loop if both pipes are closed
+            if not pipes:
+                break
+        # Wait for the process to finish
+        process.wait()
 
 def schedule_tasks(cron_expression):
     base_time = datetime.datetime.now()
